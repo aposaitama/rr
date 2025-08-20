287e74425cb65b0f7672c603bcf661f10a7b85a7
aposaitama <aposaitama@gmail.com>
2025-08-20 19:19:06 +0300

    update android version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,22 +8,19 @@ plugins {
 android {
     namespace = "com.example.rr"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973" // <- обновлено на требуемую версию
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.rr"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,8 +29,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -41,4 +36,9 @@ android {
 
 flutter {
     source = "../.."
+}
+
+// Дополнительно подавляем предупреждения об устаревшем Java source/target
+tasks.withType<JavaCompile> {
+    options.compilerArgs.add("-Xlint:-options")
 }
